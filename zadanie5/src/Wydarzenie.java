05b1db3939afa841df4e46fc3706ad577b33df7c
s33128 <s33128@pjwstk.edu.pl>
2025-04-17 00:50:23 +0200

    klasa wydarzenie

---
+++ zadanie5/src/Wydarzenie.java
@@ -0,0 +1,74 @@
+public class Wydarzenie {
+    private String nazwa;
+    private String data;
+    private String miejsce;
+    private int maxLiczbaMiejsc = 100;
+    private int dostepneMiejsca = 0;
+    private double cena;
+
+    public Wydarzenie(String nazwa, double cena) {
+        this.nazwa = nazwa;
+        this.cena = cena;
+    }
+    public Wydarzenie(String nazwa, double cena,String data) {
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.data = data;
+    }
+    public Wydarzenie(String nazwa, double cena, String data, String miejsce) {
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.data = data;
+        this.miejsce = miejsce;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getMiejsce() {
+        return miejsce;
+    }
+
+    public void setMiejsce(String miejsce) {
+        this.miejsce = miejsce;
+    }
+
+    public int getMaxLiczbaMiejsc() {
+        return maxLiczbaMiejsc;
+    }
+
+    public void setMaxLiczbaMiejsc(int maxLiczbaMiejsc) {
+        this.maxLiczbaMiejsc = maxLiczbaMiejsc;
+    }
+
+    public int getDostepneMiejsca() {
+        return dostepneMiejsca;
+    }
+
+    public void setDostepneMiejsca(int dostepneMiejsca) {
+        this.dostepneMiejsca = dostepneMiejsca;
+    }
+
+    public double getCena() {
+        return cena;
+    }
+
+    public void setCena(double cena) {
+        this.cena = cena;
+    }
+
+
+}
